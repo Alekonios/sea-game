cf8cc77e8c936bda07202af08dabb4d6b1b31192
Alekonios <alekongames@gmail.com>
2024-09-09 17:48:54 +0300

    new sky and water and graphic

--- scenes/charapters/mobs/fish/peace/test_fish.gd
+++ scenes/charapters/mobs/fish/peace/test_fish.gd
@@ -1,12 +1,23 @@
 extends CharacterBody3D
-#
-#
-#var speed 
-#var jump_vel
-#
-#enum States {Ilde, Swim, FastSwim, Jump, Scary}
-#
-#@export
-#
-#
-#@export var state = States.Ilde
+
+
+var speed = 5.0
+var jump_vel = 1.0
+
+
+@export var target : Node3D
+@onready var navagent = $NavigationAgent3D
+
+enum States {Ilde, Swim, FastSwim, Jump, Scary}
+
+@export var state = States.Ilde
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector3.ZERO
+	var pos = global_position
+	navagent.set_target_position(target.global_transform.origin)
+	var next_pos = navagent.get_next_path_position()
+	velocity = (next_pos - global_transform.origin).normalized() * speed
+	look_at(navagent.get_next_path_position())
+	move_and_slide()
+	
