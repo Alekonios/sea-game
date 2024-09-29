336b915560cd29c044ebac97966cebb029d4039c
sudo-m3zim <nizilplay2004@gmail.com>
2024-09-29 14:22:05 +0300

    Fixed water in Hold

--- scenes/ship/ship.gd
+++ scenes/ship/ship.gd
@@ -1,11 +1,11 @@
 class_name SHIP
 
 extends RigidBody3D
+
 @export var target_position : Vector3
 @export var target_rotation : Vector3
 @export var targer_angular_velocity : Vector3
 @export var targer_linear_velocity : Vector3
-
 
 @export var float_force := 1.0
 @export var water_drag := 0.05
@@ -14,9 +14,10 @@ extends RigidBody3D
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var water = get_node('/root/Node3D/Water')
 @onready var probes = $ProbeContainer.get_children()
+@onready var enter_hold_area: Area3D = $EnterHoldArea
 
 var player = false
-#1
+
 @onready var pos
 
 var SPEED = 0.0
@@ -28,16 +29,18 @@ enum States {Idle, Swim}
 
 var submerged := false
 
+func _ready() -> void:
+	enter_hold_area.body_entered.connect(enter_hold_area_entered)
+
 func sync(delta):
 	if multiplayer.is_server():
 		target_position = global_position
 		target_rotation = global_rotation
-		
+
 	else:
 		global_position = global_position.lerp(target_position, delta * 3)
 		global_rotation.y = lerp_angle(global_rotation.y, target_rotation.y, delta * 8)
 
-		
 func _physics_process(delta):
 	sync(delta)
 	moving(delta)
@@ -63,8 +66,17 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 	if submerged:
 		state.linear_velocity *=  1 - water_drag
 		state.angular_velocity *= 1 - water_angular_drag 
-	
 
 func _on_apply_vel_player_area_entered(area: Area3D) -> void:
-	
 	player = area.get_parent()
+
+func enter_hold_area_entered(body: Node3D) -> void: #Абстракция для тестировки
+	if !(body is Movement): # TODO добавить вместо Area3D анимацию или что-то вроде того
+		return
+
+	var water_level: int = 2
+	invert_water_draw(body, water_level)
+
+func invert_water_draw(player: CharacterBody3D, water_layer: int) -> void: #Функция для инвертирования слоя отрисовки
+	var current_mask_value: bool = player.camera_cur.get_cull_mask_value(water_layer) #По хорошему игрок должен быть в
+	player.camera_cur.set_cull_mask_value(water_layer, !current_mask_value) #системе
